3c51f7bdf4ff4a4f0fd0b759fa298c413547e323
ozzy <ozzy@cydeo.com>
2022-08-23 13:04:11 -0400

    Java11

--- m8-new-version-updates/src/main/java/com/cydeo/java10/OrElseThrowDemo.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java10/OrElseThrowDemo.java
@@ -17,5 +17,8 @@ public class OrElseThrowDemo {
         }
 
         System.out.println(str.orElseThrow());
+        System.out.println(str.orElseThrow(() -> new RuntimeException()));
+
+
     }
 }
